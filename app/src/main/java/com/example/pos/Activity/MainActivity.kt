e8cd76922b64e0e82173d564fe702fbdf2676866
Eka Yulianto <thepilotrock07@gmail.com>
2022-10-19 10:37:32 +0700

    initial commit

---
+++ app/src/main/java/com/example/pos/Activity/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.pos.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pos.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+
+
+    }
+}
