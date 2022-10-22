df698f97a2c8a0f9b567eb6b5716a986e05c5050
TYO <susatyo441@gmail.com>
2022-10-22 10:55:12 +0700

    setting aplikasi

---
+++ app/src/main/java/com/example/pos/Activity/settingAplikasi.kt
@@ -0,0 +1,12 @@
+package com.example.pos.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pos.R
+
+class settingAplikasi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting_aplikasi)
+    }
+}
