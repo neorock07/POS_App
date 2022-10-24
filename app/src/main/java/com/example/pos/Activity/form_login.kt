b99a1c43ac5505a43f7933f7c43af7832a52fa92
kahfi <ash.khf09@gmail.com>
2022-10-24 20:08:29 +0700

    a

---
+++ app/src/main/java/com/example/pos/Activity/form_login.kt
@@ -0,0 +1,12 @@
+package com.example.pos.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pos.R
+
+class form_login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_login)
+    }
+}
