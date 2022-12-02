ff9684f85089f6fa67480bade7078915fa01e7e8
Rakhmat Satriadi <sparkylegendary12@gmail.com>
2022-12-02 16:02:36 +0700

    saya membuat halaman edit barang

---
+++ app/src/main/java/com/example/pos/Activity/Edit_Barang.kt
@@ -0,0 +1,13 @@
+package com.example.pos.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pos.R
+
+class Edit_Barang : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_barang)
+
+    }
+}
