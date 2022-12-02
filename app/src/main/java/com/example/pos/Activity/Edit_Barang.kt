a52b78f5217623341377593c74aad18e63fe4b08
Zulfa <zulfanovita04@gmail.com>
2022-12-02 16:11:27 +0700

    aku buat fungsi tombol kembali edit barang

--- app/src/main/java/com/example/pos/Activity/Edit_Barang.kt
+++ app/src/main/java/com/example/pos/Activity/Edit_Barang.kt
@@ -2,12 +2,16 @@ package com.example.pos.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import com.example.pos.R
 
 class Edit_Barang : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_barang)
-
+        val kembali: ImageView = findViewById(R.id.kembali)
+        kembali.setOnClickListener{
+            onBackPressed()
+        }
     }
 }
